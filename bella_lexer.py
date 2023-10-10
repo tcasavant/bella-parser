3744d9a0c6e6c39a299814e91a07079118c789ac
tcasavant <tsc061003@gmail.com>
2023-10-10 00:04:44 -0400

    Renamed files to prevent collisions with built-in token.py

--- bella_lexer.py
+++ bella_lexer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import token1
+import bella_token
 import re
 
 class Lexer:
@@ -14,7 +14,14 @@ class Lexer:
         "CURLY_BRACE":"^(\\{|\\})",
         "SEMICOLON":"^;",
         "COMMA":"^,",
-        "OPERATOR":"^(\\+|\\-|\\*|\\/|\\%|\\*\\*|\\=|\\|\\||\\&\\&|\\!|\\?|\\:|\\<|\\<\\=|\\>|\\>\\=|\\=\\=|\\!\\=)",
+        # "OPERATOR":"^(\\+|\\-|\\*|\\/|\\%|\\*\\*|\\=|\\|\\||\\&\\&|\\!|\\?|\\:|\\<|\\<\\=|\\>|\\>\\=|\\=\\=|\\!\\=)",
+        "OPERATOR":"^(\\-|\\!|\\?|\\:)",
+        "OPERATOR1":"^(\\|\\|)",
+        "OPERATOR2":"^(\\&\\&)",
+        "OPERATOR3":"^(\\<\\=|\\<||\\=\\=|\\!\\=|\\>\\=|\\>)",
+        "OPERATOR4":"^(\\+|\\-)",
+        "OPERATOR5":"^(\\*|\\/|\\%)",
+        "OPERATOR6":"^(\\*\\*)",
         "FLOAT":"^\\d+((\\.\\d+)|((E|e)(\\+|\\-)?\\d+))",
         "INT":"^\\d+",
         "ID":"^[a-zA-Z]([a-zA-Z]|\\d|\\_)*",
@@ -25,6 +32,7 @@ class Lexer:
         self.position = 0
         self.tokens = []
 
+    # Convert the input string into a list of token objects
     def tokenize(self):
         while self.position < len(self.input):
             remaining_input = self.input[self.position:]
@@ -34,14 +42,14 @@ class Lexer:
             for token_type, regex in self.token_type_patterns.items():
                 match = re.search(regex, remaining_input)
                 if match:
-                    token = token1.Token(token_type, match.group())
+                    token = bella_token.Token(token_type, match.group())
                     if token.type != "WHITESPACE":
                         self.tokens.append(token)
                     self.position += len(match.group())
                     valid_token = True
                     break
             if not valid_token:
-                return
+                raise Exception("Lexer error")
 
 
         return self.tokens
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-import lexer
+import bella_lexer
 
-program = "a     =   (5 + 3.0) * 2e-2;"
-program_file = open("program.bla", "r")
+# program = "a     =   (5 + 3.0) * 2e-2;"
+program_file = open("bella_program.bla", "r")
 program = program_file.read()
 
-lexer = lexer.Lexer(program)
+lexer = bella_lexer.Lexer(program)
 tokens = lexer.tokenize()
 
-for token in tokens:
-    print(f'{token.type}: {token.value}')
+if tokens != None:
+    for token in tokens:
+        print(f'{token.type}: {token.value}')
